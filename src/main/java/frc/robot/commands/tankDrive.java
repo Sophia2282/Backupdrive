70ca7e25bfac4eea541df3e57d8ffe7ea997e3ea
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-03-09 20:10:04 -0500

    Update tankDrive.java

--- src/main/java/frc/robot/commands/tankDrive.java
+++ src/main/java/frc/robot/commands/tankDrive.java
@@ -3,30 +3,20 @@ package frc.robot.commands;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.Constants;
 import frc.robot.Robot;
-//import frc.robot.Constants;
+
 import frc.robot.RobotContainer;
-//import frc.robot.subsystems.*;
-//import frc.robot.subsystems.drivetrain;
-//import frc.robot.*;
 import edu.wpi.first.wpilibj.XboxController;
-
-//import java.util.function.DoubleSupplier;
-
-//import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-//import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class tankDrive extends CommandBase {
 
-  XboxController driverController = new XboxController(0);
-  
   /** Creates a new ArcadeDrive. */
   public tankDrive() {
     addRequirements(RobotContainer.DriveTrain);
   }
 
-
-// Called when the command is initially scheduled.
+  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
   }
@@ -34,16 +24,19 @@ public class tankDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    RobotContainer.DriveTrain.setSpeed(-driverController.getRawAxis(1), driverController.getRawAxis(4));
-    double moveSpeed = -RobotContainer.driverController.getRawAxis(1);
-    double rotateSpeed = RobotContainer.driverController.getRawAxis(4);
-    diff.tankDrive(moveSpeed, rotateSpeed);
+    /*
+     * RobotContainer.DriveTrain.setSpeed(-driverController.getRawAxis(1),
+     * driverController.getRawAxis(4));
+     * double moveSpeed = -RobotContainer.driverController.getRawAxis(1);
+     * double rotateSpeed = RobotContainer.driverController.getRawAxis(4);
+     */
+
   }
-  
+
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    diff.tankDrive(0,0);
+    diffDrive.set(0, 0);
   }
 
   // Returns true when the command should end.
@@ -52,3 +45,4 @@ public class tankDrive extends CommandBase {
     return false;
   }
 }
+
