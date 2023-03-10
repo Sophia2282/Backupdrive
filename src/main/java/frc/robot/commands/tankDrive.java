f83f46aef965391a169123891ff1fe2d382bdce5
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-03-09 22:21:53 -0500

    Update tankDrive.java

--- src/main/java/frc/robot/commands/tankDrive.java
+++ src/main/java/frc/robot/commands/tankDrive.java
@@ -1,65 +1,26 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.Constants;
-import frc.robot.Robot;
-
 import frc.robot.RobotContainer;
-import edu.wpi.first.wpilibj.XboxController;
+import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class tankDrive extends CommandBase {
-
   /** Creates a new ArcadeDrive. */
-  public tankDrive() {
-    addRequirements(DriveTrain);
+  private final DriveTrain m_Drive;
+  private final DoubleSupplier m_forward;
+  private final DoubleSupplier m_turn;
+
+  public tankDrive(DriveTrain m_driveTrain, DoubleSupplier forward, DoubleSupplier turn) {
+    m_Drive = m_driveTrain;
+    m_forward = forward;
+    m_turn = turn;
+    addRequirements(m_Drive);
   }
 
-  package frc.robot.commands;
-
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.Constants;
-import frc.robot.Robot;
-
-import frc.robot.RobotContainer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Subsystem;
-
-public class tankDrive extends CommandBase {
-
-  /** Creates a new ArcadeDrive. */
-  public tankDrive() {
-    addRequirements(DriveTrain);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    /*
-     * RobotContainer.DriveTrain.setSpeed(-driverController.getRawAxis(1),
-     * driverController.getRawAxis(4));
-     * double moveSpeed = -RobotContainer.driverController.getRawAxis(1);
-     * double rotateSpeed = RobotContainer.driverController.getRawAxis(4);
-     */
+    m_Drive.tankDrive(m_forward.getAsDouble(), m_turn.getAsDouble());
 
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }
